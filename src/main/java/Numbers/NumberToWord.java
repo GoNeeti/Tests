339b94dab10c0934838272b9d2c1204bfb2a94cf
Neeti <neetigya.goel@gmail.com>
2021-07-15 00:04:49 +0530

    Checking authentication

--- src/main/java/Numbers/NumberToWord.java
+++ src/main/java/Numbers/NumberToWord.java
@@ -4,6 +4,8 @@ public class NumberToWord {
     public static void main(String[] args) {
         int n=65;int digit=1;char chr= 'a';
         while (n>0){
+
+            
             digit=n%10;
 
         }
